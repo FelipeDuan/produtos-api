3d664ddb580df7089a4a0b2d599c187e9ae4e152
FelipeDuan <felipeduan1@gmail.com>
2025-07-06 22:49:57 -0300

    feat: persistindo dados no DB

---
+++ src/main/java/io/github/felipeduan/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package io.github.felipeduan.produtosapi.repository;
+
+import io.github.felipeduan.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+}
